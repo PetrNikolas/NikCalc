700afe99cf45a35f4036d9b650f78d93b8005020
Petr Nikolas Prokop <elprokop7@gmail.com>
2016-07-17 12:45:05 +0200

    initial commit

---
+++ app/src/main/java/cz/petrnikolas/nikcalc/MainActivity.java
@@ -0,0 +1,13 @@
+package cz.petrnikolas.nikcalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
